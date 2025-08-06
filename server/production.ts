dadcc33c20e31ce76f037f9f232d3c05b84bb5c9
ParkerPerry <perrypark24@gmail.com>
2025-08-05 21:15:20 -0700

    Fix __dirname issue in production server

--- server/production.ts
+++ server/production.ts
@@ -6,6 +6,10 @@ import { registerRoutes } from "./routes";
 import { testDbConnection } from "./db";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
